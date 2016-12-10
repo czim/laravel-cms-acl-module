a396a5a743019a15bce013eca82a0d52e9b82d61
Coen Zimmerman <coen@pixelindustries.com>
2016-12-10 22:01:50 +0100

    Permission repository now uses module manager to get all available permissions

--- src/Repositories/PermissionRepository.php
+++ src/Repositories/PermissionRepository.php
@@ -3,6 +3,7 @@ namespace Czim\CmsAclModule\Repositories;
 
 use Czim\CmsAclModule\Contracts\PermissionRepositoryInterface;
 use Czim\CmsCore\Contracts\Auth\AuthenticatorInterface;
+use Czim\CmsCore\Contracts\Modules\ModuleManagerInterface;
 
 class PermissionRepository implements PermissionRepositoryInterface
 {
@@ -13,11 +14,18 @@ class PermissionRepository implements PermissionRepositoryInterface
     protected $auth;
 
     /**
-     * @param AuthenticatorInterface $auth
+     * @var ModuleManagerInterface
      */
-    public function __construct(AuthenticatorInterface $auth)
+    protected $modules;
+
+    /**
+     * @param AuthenticatorInterface $auth
+     * @param ModuleManagerInterface $modules
+     */
+    public function __construct(AuthenticatorInterface $auth, ModuleManagerInterface $modules)
     {
-        $this->auth = $auth;
+        $this->auth    = $auth;
+        $this->modules = $modules;
     }
 
 
@@ -28,11 +36,21 @@ class PermissionRepository implements PermissionRepositoryInterface
      */
     public function getAll()
     {
-        $permissions = $this->auth->getAllPermissions();
+        $permissions = $this->modules->getAllPermissions();
 
         $permissions = array_merge($permissions, $this->getCustom());
 
         return array_unique($permissions);
+    }
+
+    /**
+     * Returns a list of all permissions currently assigned to roles and/or users.
+     *
+     * @return string[]
+     */
+    public function getAllInUse()
+    {
+        return $this->auth->getAllPermissions();
     }
 
     /**
